eecba790737f5fee92335196af3c7b325b344955
Rin18 <106043571+Rin18@users.noreply.github.com>
2026-02-03 06:26:02 +0800

    Add comments

--- main.go
+++ main.go
@@ -9,27 +9,30 @@ import (
 func get_response(endpoint string) (string, error) {
 	baseURL := "https://fapi.binance.com"
 	fullURL := baseURL + endpoint
-
 	resp, err := http.Get(fullURL)
-
+	// check error
 	if err != nil {
 		return "", err
 	}
+	//close connection
 	defer resp.Body.Close()
-
+	//check error
 	if resp.StatusCode != 200 {
 		return "", fmt.Errorf("Status error:", resp.Status)
 	}
 
 	body, err := io.ReadAll(resp.Body)
+	//check error
 	if err != nil {
 		return "", err
 	}
+
 	return string(body), err
 }
 
 func main() {
 	response, err := get_response("/fapi/v1/ping")
+	//check error
 	if err != nil {
 		fmt.Println("Request failed, error:", err)
 	}
